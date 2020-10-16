4c0a1eb10bbd807c1f5e9bc02b88bf88514470f7
Nikita Kirav <hazard3d@mail.ru>
2020-10-16 17:19:35 +0300

    Changed the static connection url to dynamic URL(independent from the server url)

--- src/api/base-url.js
+++ src/api/base-url.js
@@ -1,9 +1,7 @@
 export const baseUrl = () => {
-    console.log(process.env.NODE_ENV);
     if(process.env.NODE_ENV === 'production') {
-        return "http://54.93.233.204:1550";
+        return window.location.origin + ":1550";
     } else {
         return "http://localhost:58963";
-        //return "http://54.93.233.204:1550";
     }    
 } 
